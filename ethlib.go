72a6a319daad83134e9df4c117ffce45aad10167
snakoner <andrstroganov@yandex.ru>
2026-06-27 11:24:33 +0300

    fix wait tx function

--- ethlib.go
+++ ethlib.go
@@ -329,6 +329,74 @@ func (c *Client) TransferToken(
 	return txHashHex, nil
 }
 
+func (c *Client) GetTransactionStatus(
+	ctx context.Context,
+	txHash string,
+) (string, error) {
+	var receipt struct {
+		Status string `json:"status"`
+	}
+
+	err := c.rpcCall(ctx, "eth_getTransactionReceipt", []any{txHash}, &receipt)
+	if err != nil {
+		return "", err
+	}
+
+	if receipt.Status == "" {
+		return TransactionStatusPending, nil
+	}
+
+	statusBig, err := ParseHexBigInt(receipt.Status)
+	if err != nil {
+		return "", fmt.Errorf("parse transaction status: %w", err)
+	}
+
+	switch statusBig.Uint64() {
+	case 1:
+		return TransactionStatusSuccess, nil
+	case 0:
+		return TransactionStatusFailed, nil
+	default:
+		return "", fmt.Errorf("unexpected transaction status: %s", receipt.Status)
+	}
+}
+
+func (c *Client) WaitForTransactionSuccess(
+	ctx context.Context,
+	txHash string,
+	maxWaitTime time.Duration,
+) error {
+	ctx, cancel := context.WithTimeout(ctx, maxWaitTime)
+	defer cancel()
+
+	ticker := time.NewTicker(pollingInterval)
+	defer ticker.Stop()
+
+	for {
+		// if rpc call fails, we continue waiting
+		status, err := c.GetTransactionStatus(ctx, txHash)
+		if err == nil {
+			switch status {
+			case TransactionStatusSuccess:
+				return nil
+
+			case TransactionStatusFailed:
+				return fmt.Errorf("transaction failed: %s", txHash)
+
+			case TransactionStatusPending:
+			default:
+				return fmt.Errorf("unexpected transaction status: %s", status)
+			}
+		}
+
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("wait transaction timeout: %w", ctx.Err())
+		case <-ticker.C:
+		}
+	}
+}
+
 func (c *Client) WaitForStatusSuccess(
 	ctx context.Context,
 	txHash string,
